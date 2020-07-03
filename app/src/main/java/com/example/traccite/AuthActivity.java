275ea9c2d745fa741e8b2910eb5faaeae7078070
Aaron Teo <aaron.teo@riv-alumni.com>
2020-07-03 23:05:23 +0800

    Added FirebaseUI

---
+++ app/src/main/java/com/example/traccite/AuthActivity.java
@@ -0,0 +1,83 @@
+package com.example.traccite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthActivity extends AppCompatActivity {
+  // Firebase: Random arbitrary request code
+  private static final int RC_SIGN_IN = 1422;
+
+  // Logcat: Used for logging
+  private static final String TAG = "AuthActivity";
+
+  // Firebase: Default phone number
+  // TODO: Remove in production.
+  private static final String DEFAULT_NUMBER = "+6531235617";
+
+  // Firebase: Privacy Policy & Terms Of Service
+  private static final String TOS_URL = "https://google.com";
+  private static final String PRIVACY_POLICY_URL = "https://ite.edu.sg";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_auth);
+
+    // Show the login options
+    createSignInIntent();
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == RC_SIGN_IN) {
+      IdpResponse response = IdpResponse.fromResultIntent(data);
+
+      if (resultCode == RESULT_OK) {
+        Log.d(TAG, "Successfully signed in as: " + response.getPhoneNumber());
+      } else {
+        Log.d(TAG, "Unable to sign in");
+      }
+    }
+  }
+
+  public void createSignInIntent() {
+    List<String> countriesEnabled = Arrays.asList(
+      "SG"
+    );
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+      new AuthUI.IdpConfig.EmailBuilder().build(),
+      new AuthUI.IdpConfig.PhoneBuilder()
+        .setDefaultNumber(DEFAULT_NUMBER)
+        .setWhitelistedCountries(countriesEnabled)
+        .build()
+    );
+
+    startActivityForResult(
+      AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .setLogo(R.drawable.common_google_signin_btn_icon_dark_focused)
+        .setTheme(R.style.AuthTheme)
+        .setTosAndPrivacyPolicyUrls(
+          TOS_URL,
+          PRIVACY_POLICY_URL
+        )
+        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+        .build(),
+      RC_SIGN_IN
+    );
+  }
+}
--- app/src/main/java/com/example/traccite/MainActivity.java
+++ app/src/main/java/com/example/traccite/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.traccite;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,63 +10,74 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-    EditText Name, Phone;
-    Button Confirm;
-    int REQUEST_ENABLE_BT = 0;
+  EditText Name, Phone;
+  Button Confirm;
+  int REQUEST_ENABLE_BT = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // Get permissions
-        BluetoothAdapter BluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        if(BluetoothAdapter == null){
-            // Phone does not support bluetooth
-            alertDialogBuilder.setMessage("Your Phone does not support bluetooth!");
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-        }else {
-            if (!BluetoothAdapter.isEnabled()) {
-                Intent itEnableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(itEnableBluetooth, REQUEST_ENABLE_BT);
-            }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Intent intent = new Intent().setClass(
+      MainActivity.this,
+      AuthActivity.class
+    );
+
+    startActivity(intent);
+    finish();
+
+    // Get permissions
+    BluetoothAdapter BluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+    if (BluetoothAdapter == null) {
+      // Phone does not support bluetooth
+      alertDialogBuilder.setMessage("Your Phone does not support bluetooth!");
+      AlertDialog alertDialog = alertDialogBuilder.create();
+      alertDialog.show();
+    } else {
+      if (!BluetoothAdapter.isEnabled()) {
+        Intent itEnableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(itEnableBluetooth, REQUEST_ENABLE_BT);
+      }
+    }
+
+    // Adding items into Spinner(Combo Box)
+    String[] arraySpinner = new String[]{
+      "Resident", "Non-Resident"
+    };
+    final Spinner s = (Spinner) findViewById(R.id.spinner);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+      android.R.layout.simple_spinner_item, arraySpinner);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    s.setAdapter(adapter);
+
+    Name = findViewById(R.id.edittxtName);
+    Phone = findViewById(R.id.editTxtPhone);
+    Confirm = findViewById(R.id.btnComfirm);
+
+    // Will run the commands when "Comfirm" Button is pressed
+    Confirm.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (Phone.length() == 8 && Name.length() != 0) {
+          // get Resident/Non-Resident- can put not selected as default
+          s.getCount();
+          // get name
+          Name.getText();
+          // get number- check if number is 8 digits long
+          Phone.getText();
+        } else {
+          Phone.setText("");
+          Phone.setHint("Invalid Number!");
+
         }
 
-        // Adding items into Spinner(Combo Box)
-        String[] arraySpinner = new String[]{
-                "Resident", "Non-Resident"
-        };
-        final Spinner s = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-
-        Name = findViewById(R.id.edittxtName);
-        Phone = findViewById(R.id.editTxtPhone);
-        Confirm = findViewById(R.id.btnComfirm);
-
-        // Will run the commands when "Comfirm" Button is pressed
-        Confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Phone.length() == 8 && Name.length() != 0){
-                    // get Resident/Non-Resident- can put not selected as default
-                    s.getCount();
-                    // get name
-                    Name.getText();
-                    // get number- check if number is 8 digits long
-                    Phone.getText();
-                }else{
-                    Phone.setText("");
-                    Phone.setHint("Invalid Number!");
-
-                }
-
-            }
-        });
-    }
+      }
+    });
+  }
 }
