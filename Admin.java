7a37d55bf5c43a9821ee656478460fbb63ff561e
javiertanjunxingsch <66809177+javiertanjunxingsch@users.noreply.github.com>
2020-07-08 15:32:22 +0800

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,16 @@
+package com.example.traccite.models;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.traccite.R;
+
+public class Admin extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_appadmin_main);
+  }
+}
