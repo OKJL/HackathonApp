c4f35aae53951e6153200b3005cb26f14ececf1f
Gereth-Lim <67635294+Gereth-Lim@users.noreply.github.com>
2020-07-13 13:06:57 +0800

    Update AdminActivity.java

--- app/src/main/java/com/example/traccite/AdminActivity.java
+++ app/src/main/java/com/example/traccite/AdminActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
+  Button btnFCM;
+  Button btnBluetooth;
 
   @NonNull
   public static Intent createIntent(@NonNull Context context) {
@@ -18,5 +22,19 @@ public class AdminActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_admin);
+
+    btnFCM.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View v) {
+        // TODO: Go the FCM page
+      }
+    });
+
+    btnBluetooth.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // TODO: GO to "Create Beacon Page
+      }
+    });
   }
 }
