6d59112447f683d30f1e7fb92b6a015c276f34bf
Aaron Teo <aaron.teo@riv-alumni.com>
2020-07-16 13:19:13 +0800

    Added specific user, not finished yet

--- app/src/main/java/com/example/traccite/NotifySpecificUserActivity.java
+++ app/src/main/java/com/example/traccite/NotifySpecificUserActivity.java
@@ -1,22 +1,24 @@
 package com.example.traccite;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class NotifySpecificUserActivity extends AppCompatActivity {
 
-    @NonNull
-    public static Intent createIntent(@NonNull Context context) {
-        return new Intent(context, NotifySpecificUserActivity.class);
-    }
+  @NonNull
+  public static Intent createIntent(@NonNull Context context) {
+    return new Intent(context, NotifySpecificUserActivity.class);
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notify_specific_user);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_notify_specific_user);
+  }
 }
